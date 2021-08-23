98788a2fe4b6dbd5b8ea0aec641ae7da2cedf27c
Documenter.jl <documenter@juliadocs.github.io>
2021-08-23 23:44:09 +0000

    build based on 0a1fef3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ProjectAssigner","category":"page"},{"location":"#ProjectAssigner","page":"Home","title":"ProjectAssigner","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProjectAssigner.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ProjectAssigner]","category":"page"}]
+}
